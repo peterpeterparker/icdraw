8fa32852cb6f5c811ec9a7a1524907b8334783a1
David Dal Busco <david.dalbusco@outlook.com>
2023-06-20 21:00:47 +0200

    build: compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteCompression()],
   worker: {
     format: "es",
   },
