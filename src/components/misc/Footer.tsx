ef877d5756154527f4a5c4e909ec2c7c5547b1b9
David Dal Busco <david.dalbusco@outlook.com>
2023-06-20 21:28:04 +0200

    feat: github link

--- src/components/misc/Footer.tsx
+++ src/components/misc/Footer.tsx
@@ -1,18 +1,31 @@
 import { IconJuno } from "../icons/IconJuno.tsx";
 import styles from "./Footer.module.scss";
+import {IconGitHub} from "../icons/IconGitHub.tsx";
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
+      <div  className={styles.juno}>
+          <a
+              className={styles.link}
+              href="https://juno.build"
+              aria-label="Build with Juno"
+              rel="noopener noreferrer"
+              target="_blank"
+          >
+              <IconJuno />
+              Build with Juno
+          </a>
+      </div>
+
       <a
-        className={styles.link}
-        href="https://juno.build"
-        aria-label="Build with Juno"
+        className={styles.github}
+        href="https://github.com/peterpeterparker/icdraw"
+        aria-label="Open source on GitHub"
         rel="noopener noreferrer"
         target="_blank"
       >
-        <IconJuno />
-        Build with Juno
+        <IconGitHub />
       </a>
     </footer>
   );
