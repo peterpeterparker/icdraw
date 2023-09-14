74cf5c3246c4421f1a1591c89833351e74538f0e
David Dal Busco <david.dalbusco@outlook.com>
2023-09-14 09:32:44 +0200

    feat: remove analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,15 @@ import { Scene } from "./components/Scene.tsx";
 import { Auth } from "./components/context/Auth.tsx";
 import { Metadata } from "./components/context/Metadata.tsx";
 import { Worker } from "./components/context/Worker.tsx";
-import { initOrbiter } from "@junobuild/analytics";
 
 const App = () => {
   const [ready, setReady] = useState(false);
 
   useEffect(() => {
     (async () => {
-      const satelliteId = "fqotu-wqaaa-aaaal-acp3a-cai";
-
-      await Promise.all([
-        initJuno({
-          satelliteId,
-        }),
-        initOrbiter({
-          satelliteId,
-          orbiterId: "3iier-sqaaa-aaaal-aczaa-cai",
-        }),
-      ]);
+      await initJuno({
+        satelliteId: "fqotu-wqaaa-aaaal-acp3a-cai",
+      })
 
       setReady(true);
     })();
